aef4f30719cf1af18c867d8c14598ad49dce2067
lvzhenchao <1370164516@qq.com>
2022-03-12 12:36:40 +0800

    Create User.php

---
+++ app/model/User.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\model;
+
+use think\Model;
+
+class User extends Model
+{
+    protected $pk = 'id';
+    // 设置当前模型对应的完整数据表名称
+    protected $table = 'qing_user';
+
+    // 设置当前模型的数据库连接
+//    protected $connection = 'db_config';
+}
